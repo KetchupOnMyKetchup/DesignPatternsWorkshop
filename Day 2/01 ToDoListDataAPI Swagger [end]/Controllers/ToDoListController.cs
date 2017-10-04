29a3d26a886247fcbc1fea4beed4eaa99c8e0a01
Crystal Tenn <crtenn@microsoft.com>
2017-10-03 22:37:17 -0400

    add labs and slides

---
+++ Day 2/01 ToDoListDataAPI Swagger [end]/Controllers/ToDoListController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ToDoListDataAPI.Models;
+using ToDoListDataAPI.Business;
+
+namespace ToDoListDataAPI.Controllers
+{
+    public class ToDoListController : ApiController
+    {
+        ToDoListManager _manager = new ToDoListManager();
+
+        // GET: api/ToDoList
+        [HttpGet]
+        public IEnumerable<ToDoItem> GetTodoItems()
+        {
+            return _manager.GetTodoItems();
+        }
+
+        // GET: api/ToDoList/5
+        [HttpGet]
+        public ToDoItem GetTodoItemById(int id)
+        {
+            return _manager.GetTodoItemById(id);
+        }
+
+        // POST: api/ToDoList
+        [HttpPost]
+        public void UpdateToDoList(ToDoItem todo)
+        {
+            _manager.InsertTodoItem(todo);
+        }
+
+        // PUT: api/ToDoList
+        [HttpPut]
+        public void Put(ToDoItem todo)
+        {
+            _manager.EditDescription(todo);
+        }
+
+        // DELETE: api/ToDoList/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _manager.DeleteById(id);
+        }
+    }
+}
+
